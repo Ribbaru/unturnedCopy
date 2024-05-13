aedbb0a6fb42110451565593bf0f28aa704c3cc7
Ribbaru <dimakvitko49050@gmail.com>
2024-05-13 17:11:40 +0300

    project upload

---
+++ FPS/Assets/Game Scene/Scripts/PlayerScripts/CharacterMovement.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    public float jumpForce = 5.0f;
+    private CharacterController characterController;
+    private Vector3 moveDirection;
+    private float verticalRotation = 0;
+    public float upDownRange = 60.0f;
+    public float rotationSpeed = 2.0f;
+
+
+    private Camera playerCamera;
+
+    private float verticalVelocity = 0;
+
+
+    private void Start()
+    {
+
+        characterController = GetComponent<CharacterController>();
+        playerCamera = GetComponentInChildren<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+
+    }
+
+    private void Update()
+    {
+        // Поворот персонажа влево и вправо (поворот за курсором)
+        float horizontalRotation = Input.GetAxis("Mouse X") * rotationSpeed;
+        transform.Rotate(0, horizontalRotation, 0);
+
+        // Поворот камеры вверх и вниз (поворот за курсором)
+        verticalRotation -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        verticalRotation = Mathf.Clamp(verticalRotation, -upDownRange, upDownRange);
+        playerCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+ 
+        // Перемещение
+        float forwardSpeed = Input.GetAxis("Vertical") * moveSpeed;
+        float sideSpeed = Input.GetAxis("Horizontal") * moveSpeed;
+
+        verticalVelocity += Physics.gravity.y * Time.deltaTime;
+
+        if (characterController.isGrounded)
+        {
+            verticalVelocity = -0.5f;
+
+            // Прыжок
+            if (Input.GetButtonDown("Jump"))
+            {
+                verticalVelocity = jumpForce;
+            }
+        }
+
+
+        // Применяем скорость передвижения к персонажу
+        moveDirection = new Vector3(sideSpeed, verticalVelocity, forwardSpeed);
+        moveDirection = transform.TransformDirection(moveDirection);
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+
+
+}
