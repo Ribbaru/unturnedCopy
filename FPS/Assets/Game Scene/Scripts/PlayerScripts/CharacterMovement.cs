5e98fdcf3e13dafaf04850dd046b6464c0ac7af9
Ribbaru <dimakvitko49050@gmail.com>
2024-05-17 17:49:06 +0300

    terrain

--- FPS/Assets/Game Scene/Scripts/PlayerScripts/CharacterMovement.cs
+++ FPS/Assets/Game Scene/Scripts/PlayerScripts/CharacterMovement.cs
@@ -13,55 +13,59 @@ public class CharacterMovement : MonoBehaviour
     public float upDownRange = 60.0f;
     public float rotationSpeed = 2.0f;
     public InventoryManager inventoryManager;
+    public float swimSpeed;
 
+    public bool inLayer3 = false; // Флаг для проверки нахождения в слое 3
 
     private Camera playerCamera;
 
     private float verticalVelocity = 0;
 
-
     private void Start()
     {
-
         characterController = GetComponent<CharacterController>();
         playerCamera = GetComponentInChildren<Camera>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-
-
     }
 
     private void Update()
     {
-
         CameraRotation();
 
         // Перемещение
         float forwardSpeed = Input.GetAxis("Vertical") * moveSpeed;
         float sideSpeed = Input.GetAxis("Horizontal") * moveSpeed;
 
-        verticalVelocity += Physics.gravity.y * Time.deltaTime;
-
-        if (characterController.isGrounded)
+        if (inLayer3 && Input.GetButton("Jump"))
         {
-            verticalVelocity = -0.5f;
+            verticalVelocity = swimSpeed; // Устанавливаем скорость подъема для полета
+        }
+        else
+        {
+            verticalVelocity += Physics.gravity.y * Time.deltaTime;
 
-            // Прыжок
-            if (Input.GetButtonDown("Jump"))
+            if (characterController.isGrounded)
             {
-                verticalVelocity = jumpForce;
+                verticalVelocity = -0.5f;
+
+                // Прыжок
+                if (Input.GetButtonDown("Jump"))
+                {
+                    verticalVelocity = jumpForce;
+                }
             }
         }
-
 
         // Применяем скорость передвижения к персонажу
         moveDirection = new Vector3(sideSpeed, verticalVelocity, forwardSpeed);
         moveDirection = transform.TransformDirection(moveDirection);
         characterController.Move(moveDirection * Time.deltaTime);
     }
+
     public void CameraRotation()
     {
-        if (inventoryManager.isOpened == true)
+        if (inventoryManager.isOpened)
         {
             return;
         }
@@ -75,8 +79,24 @@ public class CharacterMovement : MonoBehaviour
             verticalRotation -= Input.GetAxis("Mouse Y") * rotationSpeed;
             verticalRotation = Mathf.Clamp(verticalRotation, -upDownRange, upDownRange);
             playerCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
-
         }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 3)
+        {
+            inLayer3 = true;
+            moveSpeed = 15;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 3)
+        {
+            inLayer3 = false;
+            moveSpeed = 10;
+        }
+    }
 }
